6fda5b91241b4e52d3273df6247bce47239240a6
Jared Nelson <jarednelson@Jareds-MacBook-Air.local>
2020-07-04 19:42:23 -0600

    Even more

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, useCallback } from "react"
 import { Link } from "gatsby"
 import Hamburger from "../Hamburger"
+import PropTypes from "prop-types"
 import "./styles.css"
 
 const Menu = () => {
